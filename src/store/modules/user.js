7421ac65f99ac2d11427101908a7393830f6a242
itxjm <2844815353@qq.com>
2022-11-08 21:59:19 +0800

    登录页面调用登录action异常处理

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,30 @@
+import { getToken, setToken, removeToken } from "@/utils/auth";
+import { login } from "@/api/user";
+// 状态
+const state = {
+  token: getToken, //设置token为共享状态 初始化vuex的时候 先从缓存中读取
+};
+const mutations = {
+  setToken(state, token) {
+    state.token = token; //将数据设置给vuex
+    setToken(token); // 同步给缓存
+  },
+  removeToken(state) {
+    state.token = null; // 将vuex数据置空
+    removeToken(); //同步到缓存
+  },
+};
+const actions = {
+  async login(context, data) {
+    // 调用api接口
+    const result = await login(data) //拿到token
+    context.commit("setToken", result) //设置token(响应拦截器处理后的结构)
+  },
+}
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {},
+  state,
+  mutations,
+  actions,
 };
